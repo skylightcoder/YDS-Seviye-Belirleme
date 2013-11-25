7997c220ff73d1ff7e11f2d2aa402f6c1de9d596
skylightcoder <skylightcoder@gmail.com>
2013-11-25 11:08:29 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YDSSeviyeBelirleme::Application.config.secret_key_base = 'de27b6fff172d602e3baa754ac8f5e08e70a909bc01767153f445a455358b29105bec777cbdd86aa10cefde40eeb3d8cc2cec2c2172421692ee79dec514ae780'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YDSSeviyeBelirleme::Application.config.session_store :cookie_store, key: '_YDS-Seviye-Belirleme_session'
